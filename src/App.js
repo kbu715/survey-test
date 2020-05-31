9cb7b18ac0f894a2595dd11981e66a69994b8e56
kbu715 <kbu715@gmail.com>
2020-05-31 12:40:07 +0900

    front complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Sex extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sex: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      sex: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    alert(`당신은 ${this.state.sex}.`);
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>당신의 성별은?</p>
+
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="남자"
+                  checked={this.state.sex === "남자"}
+                  onChange={this.handleChange}
+                />
+            남자
+          </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="여자"
+                  checked={this.state.sex === "여자"}
+                  onChange={this.handleChange}
+                />
+            여자
+          </label>
+            </li>
+          </ul>
+
+          <button type="submit">선택</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+class Animal extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      animal: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      animal: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    alert(`당신은 ${this.state.animal}를 좋아합니다.`);
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>당신이 좋아하는 동물은?</p>
+
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="호랑이"
+                  checked={this.state.animal === "호랑이"}
+                  onChange={this.handleChange}
+                />
+            호랑이
+          </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="코끼리"
+                  checked={this.state.animal === "코끼리"}
+                  onChange={this.handleChange}
+                />
+            코끼리
+          </label>
+            </li>
+          </ul>
+
+          <button type="submit">선택</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sex></Sex>
+        <Animal></Animal>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
